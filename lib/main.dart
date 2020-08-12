ea54abba06bfeda630c5cb767ee43c76a2f1a877
yankev <yankevmineo@gmail.com>
2020-08-12 20:29:29 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,217 @@
+import 'dart:convert';
+
+import 'package:first_project/models/item.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+var borderRounded =
+    new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10));
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  var items = List<Item>();
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  _HomePageState() {
+    load();
+  }
+
+  Future load() async {
+    var prefs = await SharedPreferences.getInstance();
+    var data = prefs.getString("data");
+    if (data != null) {
+      Iterable decoded = jsonDecode(data);
+      List<Item> result =
+          decoded.map((itemJson) => Item.fromJson(itemJson)).toList();
+      setState(() {
+        widget.items = result;
+      });
+    }
+  }
+
+  save() async {
+    var prefs = await SharedPreferences.getInstance();
+    await prefs.setString("data", jsonEncode(widget.items));
+  }
+
+  void addTask(String nametask) {
+    setState(() {
+      widget.items.add(Item(title: nametask, done: false));
+      save();
+    });
+  }
+
+  void removeTask(int index) {
+    setState(() {
+      widget.items.removeAt(index);
+      save();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Desafios aceitos"
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text("Lista que eu termino com os olhos fechados"),
+          ListView.builder(
+            itemCount: widget.items.length,
+            shrinkWrap: true,
+            itemBuilder: (BuildContext context, int index) {
+              var item = widget.items[index];
+              return Dismissible(
+                key: Key(item.title),
+                background: Container(
+                  color: Colors.green.withOpacity(0.2),
+                  padding: EdgeInsets.only(left: 10),
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Concluído!',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                secondaryBackground: Container(
+                  color: Colors.green.withOpacity(0.2),
+                  padding: EdgeInsets.only(right: 10),
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    'Finalizado!',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                onDismissed: (direction) {
+                  removeTask(index);
+                  final snackBar = SnackBar(
+                    content: Text('Manda outra que essa foi fácil'),
+                    action: SnackBarAction(
+                      label: 'fechar',
+                      onPressed: () {
+                        // Some code to undo the change.
+                        Scaffold.of(context).hideCurrentSnackBar();
+                      },
+                    ),
+                  );
+                  Scaffold.of(context).showSnackBar(snackBar);
+                },
+                child: CheckboxListTile(
+                  title: Text(item.title),
+                  key: Key(item.title),
+                  value: item.done,
+                  onChanged: (value) {
+                    setState(
+                      () {
+                        item.done = value;
+                        save();
+                      },
+                    );
+                  },
+                ),
+              );
+            },
+          ),
+          Text('Lista que eu terminei com sucesso')
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        key: Key("button"),
+        child: Icon(
+          Icons.add,
+          size: 50,
+          color: Colors.white,
+        ),
+        onPressed: () async {
+          String result = await _incrementTask(context);
+          if (result != null && result.isNotEmpty) {
+            addTask(result);
+          }
+        },
+      ),
+    );
+  }
+}
+
+Future<String> _incrementTask(context) async {
+  var newTask = TextEditingController();
+  return showDialog(
+    context: context,
+    builder: (BuildContext bc) {
+      return SimpleDialog(
+        shape: borderRounded,
+        title: Text(
+          "O que nós vamos fazer?",
+          style: TextStyle(fontSize: 15, color: Colors.black.withOpacity(0.7)),
+          textAlign: TextAlign.left,
+        ),
+        contentPadding: EdgeInsets.all(10),
+        children: <Widget>[
+          TextFormField(
+            keyboardType: TextInputType.text,
+            controller: newTask,
+            decoration: new InputDecoration(
+              labelText: 'título',
+              fillColor: Colors.white,
+              border: new OutlineInputBorder(
+                borderRadius: new BorderRadius.circular(15.0),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                RaisedButton(
+                  child: Text('Cancelar'),
+                  textColor: Colors.white,
+                  color: Colors.blue[400],
+                  shape: borderRounded,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    Navigator.pop(context, newTask.text);
+                  },
+                  textColor: Colors.blue[400],
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      side: BorderSide(color: Colors.blue[400], width: 3)),
+                  child:
+                      Text('OK', style: TextStyle(fontWeight: FontWeight.bold)),
+                ),
+              ],
+            ),
+          ),
+        ],
+      );
+    },
+  );
+}
